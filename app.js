65edea68b5dddbcaaa225cc8956c1a68ace2a6ab
jfcarocota <jfcarocota@gmail.com>
2019-09-16 19:18:14 -0700

    pokemon data added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const mainContainer = document.getElementById('main-container');
 const apiUrl = 'https://pokeapi.co/api/v2/';
+
+let getPokemonData = async ()=>{
+    const response = await fetch(`${apiUrl}pokemon/?limit=150&offset=10"`);
+    const data = await response.json();
+    console.log(data);
+};
+
+getPokemonData();
